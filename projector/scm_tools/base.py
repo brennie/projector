3590b4d6b467db2258c4e1a265be202cad918a32
Barret Rennie <barret@brennie.ca>
2018-09-27 18:15:51 -0400

    Add module-level docs to projector.scm_tools.base

--- projector/scm_tools/base.py
+++ projector/scm_tools/base.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Base classes for SCM tools."""
+
 from typing import Type
 
 from marshmallow import Schema
